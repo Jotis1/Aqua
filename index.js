589ec5dbf46aac3b010b65220fe499d9282e6892
Jotis1 <74616896+Jotis1@users.noreply.github.com>
2021-01-04 02:20:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 
 
-
+const prefix = "aq!"
 
 
 client.on("ready", () => {
@@ -18,5 +18,23 @@ client.on("ready", () => {
     })
 
 })
+
+
+client.on("message", async (message) => {
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+
+    if (command === "omedetou"){
+        message.react('☑️')
+        message.channel.send(`Omedetou. Has completado mi evento. ¿Te ha gustado como me ha quedado? Espero que no haya sido muy difícil... :two_hearts:`)
+    }
+
+
+
+
+})
+
 
 client.login(process.env.TOKEN)
